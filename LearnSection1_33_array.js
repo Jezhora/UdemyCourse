f0f39a4d40ae942c3362e810ec4c828b579deedd
Jezhora <jezhora.photo@gmail.com>
2023-04-18 18:00:58 +0300

    feat: edit section1_33

--- LearnSection1_33_array.js
+++ LearnSection1_33_array.js
@@ -25,11 +25,33 @@ arr.forEach(function(item, index, arr){
 });
 
 
+
 // If we use for/of we can use "break" & "continue"
 // for (let value of arr) {
 //     console.log(value)
 // }
 
+
+// .split()
+// takes a pattern and divides a String into an ordered list
+//  of substrings by searching for the pattern, 
+//  puts these substrings into an array, and returns the array.
+
+// .join()
+// method creates and returns a new string by concatenating all 
+// of the elements in an array
+
+// .sort()
+// method sorts the elements of an array in place 
+// and returns the reference to the same array, now sorted. 
+// The default sort order is ascending, built upon converting
+//  the elements into strings, then comparing their sequences
+//   of UTF-16 code units values.
+function compareNum(a, b) {
+    return a - b;
+}
+
+
 const str = prompt('','');
 const products = str.split(', ');
-console.log(products);
+console.log(products.join('; '));
