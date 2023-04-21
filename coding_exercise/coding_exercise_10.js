0f6095e0e42f97184ced5c831a5943c8d7103c84
Jezhora <jezhora.photo@gmail.com>
2023-04-21 11:20:58 +0300

    feat: coding_exercise_10 done second

--- coding_exercise/coding_exercise_10.js
+++ coding_exercise/coding_exercise_10.js
@@ -22,15 +22,33 @@ const personalPlanPeter = {
 
 function showExperience(plan) {
 const {exp} = plan.skills
-return exp.toString()
+return exp;
 }
 
 
+// 2)Write the showProgrammingLangs function, which will accept an object 
+// with all the data and return a string in the required form.
+// Example:
+// showProgrammingLangs(personalPlanPeter) =>
+// "Js is 20% learned PHP is 10% learned"
+// Moreover, the function should work regardless of the number of languages.
+//  If none is specified, then an empty string is returned.
 
-
-
+// Line breaks use \n at the end of the line.
 
 
 function showProgrammingLangs(plan) {
+const lang = plan.skills.programmingLangs
 
+let answ = ''
+    if(Object.keys(lang).length === 0) {
+        return answ
+    } else {
+        for (let key in lang) {
+            answ += `Язык ${key} изучен на ${lang[k]}\n`
+        }    
+    }
+    return answ
 }
+
+console.log(showProgrammingLangs(personalPlanPeter));
