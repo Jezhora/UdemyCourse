094e239a4be45d3a082342e7b72874b72d607bfd
Jezhora <jezhora.photo@gmail.com>
2023-04-28 17:44:10 +0300

    feat: change to English and add the task

--- coding_exercise/coding_exercise_13_adv.js
+++ coding_exercise/coding_exercise_13_adv.js
@@ -43,10 +43,25 @@ function isBudgetEnough(data) {
     const shopBudget = data.budget;
 
     if(shopBudget - (shopsVolume * value) >= 0) {
-        return 'Бюджета достаточно'
+        return 'Enough budget'
     } else {
-        return 'Бюджета недостаточно'
+        return 'Budget is not enough'
     }
 
 }
 console.log(isBudgetEnough(shoppingMallData));
+
+// You have a small remnant of mall data that is parsed into a shoppingMallData object. 
+// They represent an array of data about markets, where the length
+// and width of the premises are indicated; room height;
+// the cost of heating per 1 cubic meter and the budget for paying for heating per month.
+// The main task is to write an isBudgetEnough functionthat will return a string.
+// If there is enough budget for heating the entire shopping center - 'Budget is enough'
+// is displayed, if not - 'Budget is not enough'. And that's all 🙂
+// But this problem contains several subtasks inside:
+// - calculation of the total area of all stores, which is calculated as the length of the store, multiplied by the width;
+// - calculation of the total volume of the shopping center, since the price of heating is indicated in cubic meters;
+// - determination of whether the budget is enough for expenses of such a volume;
+// - all numbers go without units for simplification, just numbers and that's it;
+// - the function should continue to work even if the number of stores, height,
+// budget is reduced, or another object is substituted.
