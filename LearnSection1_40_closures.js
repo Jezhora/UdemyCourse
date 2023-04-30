09fb48e3134b6121ae1fc2cba67a68c3e45e87d3
Jezhora <jezhora.photo@gmail.com>
2023-04-30 22:31:22 +0300

    feat: section_40_closure done

--- LearnSection1_40_closures.js
+++ LearnSection1_40_closures.js
@@ -1,12 +1,44 @@
 'use strict';
 
-let number =5; debugger
+// let number =5; debugger
 
-function logNumber() {
-    let number = 4; debugger
-    console.log(number);
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber();
+
+function createCouter() { 
+    let counter = 0;
+
+    const myFunction = function() { 
+        counter = counter + 1;
+        return counter 
+    }
+
+    return myFunction; 
 }
 
-number = 6;
+const increment = createCouter(); 
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
 
-logNumber();
+console.log(c1, c2, c3);
+
+
+{
+    let msg = 'Hello';
+}
+
+console.log(msg);
+
+for (let i = 0; i < 9; i++) {
+    for(let j = 0; j < 9; j++) {
+        let num = 3
+    }
+    console.log(num)
+}
