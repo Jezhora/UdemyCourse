de2b7654c3351fdfc978292e3e6e557695090953
Jezhora <jezhora.photo@gmail.com>
2023-05-06 14:42:43 +0200

    reat: 42_lesson done

--- LearnSection1/42_getElementFromPage.js
+++ LearnSection1/42_getElementFromPage.js
@@ -1,1 +1,26 @@
 'use strict'
+
+// get element by id
+const box = document.getElementById('box');
+console.log(box)
+
+// get element by tag name
+const btns = document.getElementsByTagName('button');
+
+console.log(btns[1])
+
+// get element by class name
+const circles = document.getElementsByClassName('circle');
+
+console.log(circles);
+
+// css selector
+const hearts = document.querySelectorAll('.heart');
+
+
+hearts.forEach(item => {
+    console.log(item)
+})
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
