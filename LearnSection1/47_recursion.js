eea5dcaf74115b38b2d66efb605d624c754a9735
Jezhora <jezhora.photo@gmail.com>
2023-05-11 18:08:30 +0300

    feat: edit indexHTML

--- LearnSection1/47_recursion.js
+++ LearnSection1/47_recursion.js
@@ -10,3 +10,14 @@ function pow (x, n) {
 
 // pow(2, 2) //4
 // pow(2, 3) //8
+//pow(2, 4) //16
+
+function pow (x, n) { debugger
+    if (n === 1) {
+        return x;
+    } else {
+        return x * pow (x, n - 1); debugger
+    }
+}
+
+console.log(pow(2, 4))
